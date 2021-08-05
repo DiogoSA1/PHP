f332f89c424c2b666c9ea377be3fcc310d2593bd
Diogo Albuquerque <albuquerquediogo@hotmail.com>
2021-08-05 11:37:54 -0300

    praticas de estrutura condicionais, loops e desafios

---
+++ Condicionais e loops/loopFor.php
@@ -0,0 +1,19 @@
+<?php
+$numero = 0;
+
+// while($numero < 10) {
+//     echo "N: ".$numero."<br/>";
+
+        //$numero++;
+//     //$numero = $numero + 1;
+//     $numero += 1;
+// }
+
+// for($numero = 0; $numero < 10; $numero += 1){
+//     echo "N: ".$numero."<br/>";
+// }
+
+
+for($i = 0; $i < 10; $i++){
+    echo "N: ".$i."<br/>";
+}
