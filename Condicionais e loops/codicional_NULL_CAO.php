56ff7665e831a4d6e15ed4079365dc90a42d3ac0
Diogo Albuquerque <albuquerquediogo@hotmail.com>
2021-08-09 21:55:58 -0300

    praticas de estruturas condicionais, loops e desafios

--- Condicionais e loops/codicional_NULL_CAO.php
+++ Condicionais e loops/codicional_NULL_CAO.php
@@ -5,7 +5,7 @@ $sobrenome = 'Albuquerque';
 // para testar os códigos abaixo comente as variaveis acima.
 $nomeCompleto = $nome;
 $nomeCompleto .= isset($sobrenome) ? $sobrenome : ''; // metoddo tradicional de condicional ternaria
-$nomeCompleto = $sobrenome ?? ''; // condicional NULL CAO
-$nomeCompleto = $nome ?? 'visitante'; 
+$nomeCompleto .= $sobrenome ?? ''; // condicional NULL CAO
+$nomeCompleto .= $nome ?? 'visitante'; 
 
 echo $nomeCompleto;
