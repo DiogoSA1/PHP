720f93c0b04da38136f0f957bf77b63f051fca46
Diogo Albuquerque <albuquerquediogo@hotmail.com>
2021-08-13 19:41:16 -0300

    excluindo arquivos com unlink()

--- Formularios, Validacoes, Sessoes e Arquivos/desafio_lista_txt/validacao.php
+++ Formularios, Validacoes, Sessoes e Arquivos/desafio_lista_txt/validacao.php
@@ -5,14 +5,17 @@ $nome= filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 ;
 
 if ($nome) {
+
+    $expiracao = time() + (86400 * 30);
+    setcookie('nome', $nome, $expiracao);
+    
     $nome = '<li>'.$nome.'</li>';
     $lista = file_get_contents('lista.txt');
     $lista .= "\n $nome";
     $lista = file_put_contents('lista.txt', $lista);
     
     header('location: index.php');
-    $expiracao = time() + (86400 * 30);
-    setcookie('nome', $nome, $expiracao);
+    
     
 } else {
 
---
+++ Formularios, Validacoes, Sessoes e Arquivos/excluindo_arquivo.php
@@ -0,0 +1,5 @@
+<?php
+
+// unlink() => função que deleta o arquivo citado no parametro da função
+unlink('lista2.txt');
+echo 'Excluido com sucesso';
