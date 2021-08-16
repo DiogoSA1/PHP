dcd8fb07b8c1ad262327b657b99a3685f20139b8
Diogo Albuquerque <albuquerquediogo@hotmail.com>
2021-08-16 16:46:37 -0300

    Descontruindo arrays com list()

---
+++ Manipulacao de Array/desconstrucao_list.php
@@ -0,0 +1,8 @@
+<?php
+$array =['Diogo', 26, 'café', 'azul'];
+
+// list() => descontroi array e armazena seu intens em variaveis conforme parametros abaixo
+list($nome, $idade, $bebida, $cor) = $array;
+
+echo $nome.' tem '.$idade.' anos e gosta de tomar '.$bebida.' com a cor '.$cor;
+
