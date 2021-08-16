91907d8a78201b10610add1bee6e614c53db89fe
Diogo Albuquerque <albuquerquediogo@hotmail.com>
2021-08-16 13:47:01 -0300

    Criando novos arrays a partir de um array existente com array_keys() e array_values()

--- Manipulacao de Array/key_exists.php
+++ Manipulacao de Array/key_exists.php
@@ -10,8 +10,22 @@ $array = [
 // key_exists() => verifica se existe uma chave especifica dentro de um array
 if(key_exists('idade', $array)) {
     $idade = $array['idade'];
-    echo $idade." anos";
+    echo $idade." anos"."</br>";
 } else {
-    echo 'Não tem idade';
+    echo 'Não tem idade'."</br>";
 }
+
+print_r($array);
+echo "</br>";
+
+
+// array_keys() => cria um novo array com as chaves do array passado no parametro
+$chaves = array_keys($array);
+print_r($chaves);
+echo "</br>";
+
+// array_values() => cria uma novo array com os valores do array passado no parametro
+$valores = array_values($array);
+print_r($valores);
+echo "</br>";
 
